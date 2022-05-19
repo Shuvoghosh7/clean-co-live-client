314a27e3951032db389f978ddae28820fb6bde17
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-20 00:43:12 +0600

    add responsive navber

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Services from './Pages/Services';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home page</h1>
+    <div >
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
